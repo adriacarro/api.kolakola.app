37c6a6d693b3c377992bd3008997d5b1e16abc2c
Adrià Carro <adria@digitalyou.es>
2021-04-16 17:39:17 +0200

    Fix address serializer

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class ServiceSerializer < ActiveModel::Serializer
+class AddressSerializer < ActiveModel::Serializer
   attributes :id, :name, :code, :street_1, :street_2, :city, :state, :zip_code, :country_code
 end
