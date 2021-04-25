ae6e4fecd3ef585be735b6c91a74ddffe9f034a0
Adrià Carro <adria@digitalyou.es>
2021-04-26 00:21:38 +0200

    Update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,11 +2,24 @@
 
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :created_at
+
+  attribute :line, if: :is_worker?
   attribute :invite_accepted, if: :is_worker?
   attribute :invited_at, if: :is_worker?
-  belongs_to :place, unless: :is_customer?
+
+  attribute :place, unless: :is_customer?
+  belongs_to :service, if: :is_worker?
+  
   attribute :cookie, if: :is_customer?
   has_many :lines, if: :is_customer?
+
+  def line
+    object.lines.active.any? ? ActiveModelSerializers::SerializableResource.new(object.lines.active.first).serializable_hash : nil
+  end
+
+  def place
+    ActiveModelSerializers::SerializableResource.new(object.place, user: object).serializable_hash
+  end
 
   def is_worker?
     object.worker?
