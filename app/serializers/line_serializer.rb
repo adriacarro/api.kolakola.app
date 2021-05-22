9d1767596302de3614b77b27e2c3f3e6a238bd62
Adrià Carro <adria@digitalyou.es>
2021-05-22 22:11:18 +0200

    Send worker name to line serializer

--- app/serializers/line_serializer.rb
+++ app/serializers/line_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class LineSerializer < ActiveModel::Serializer
-  attributes :id, :code, :position, :status, :service_id
+  attributes :id, :code, :position, :status, :service_id, :worker
+
+  def worker
+    object.worker&.name
+  end
 end
