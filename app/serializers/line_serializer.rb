a1f5c7d622dc73090656ba3bea0e68fdad9dd40a
Adrià Carro <adria@digitalyou.es>
2021-05-22 23:46:48 +0200

    Add service name to line serializer

--- app/serializers/line_serializer.rb
+++ app/serializers/line_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class LineSerializer < ActiveModel::Serializer
-  attributes :id, :code, :position, :status, :service_id, :worker
+  attributes :id, :code, :position, :status, :service_id, :worker, :service
+
+  def service
+    object.service&.name
+  end
 
   def worker
     object.worker&.name
