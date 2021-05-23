9edd73ac79076862ae85ad7456bff79f265af076
Adrià Carro <adria@digitalyou.es>
2021-05-23 12:18:31 +0200

    Add avg_serving_time to line serializer

--- app/serializers/line_serializer.rb
+++ app/serializers/line_serializer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class LineSerializer < ActiveModel::Serializer
-  attributes :id, :code, :position, :status, :service_id, :worker, :service
+  attributes :id, :code, :position, :status, :service_id, :worker, :service, :avg_serving_time
 
   def service
     object.service&.name
+  end
+
+  def avg_serving_time
+    object.service&.avg_serving_time
   end
 
   def worker
