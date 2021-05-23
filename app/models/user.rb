e325ce9f79ac3310948123906c50aed146d326af
Adrià Carro <adria@digitalyou.es>
2021-05-23 12:52:46 +0200

    call_to_next fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,7 +105,7 @@ class User < ApplicationRecord
 
   # 1st step of line handshake
   def call_to_next
-    next_to_be_served = services.map{ |service| service.lines.waiting.to_a }.flatten.shuffle.first
+    next_to_be_served = services.map{ |service| service.lines.waiting.first }.compact.shuffle.first
     return if next_to_be_served.nil? # No more attendees
 
     # Start line handshake
